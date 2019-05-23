0158dd925724c698a29b15cb4e72874109ae350c
Lostefra <lostefra@users.noreply.github.com>
2019-05-23 14:59:28 +0200

    Aggiunto main per i tre client

---
+++ ClientAddettoSicurezza/src/controller/MainClientAddettoSicurezza.java
@@ -0,0 +1,58 @@
+package controller;
+import java.net.*;
+import java.io.*;
+
+public class MainClientAddettoSicurezza {
+
+	public static void main(String[] args) throws IOException {
+		//fissi localhost 10101
+		InetAddress addr = InetAddress.getByName("localhost");
+		int port = 10101;
+
+		
+
+		// Creazione socket di I/O
+		Socket socket = null;
+		DataInputStream inSock = null;
+		DataOutputStream outSock = null;
+		BufferedReader stdIn = null;
+
+		try {
+			socket = new Socket(addr, port);
+			socket.setSoTimeout(30000); // Solo se richiesto
+			System.out.println("Creata la socket: " + socket);
+			inSock = new DataInputStream(socket.getInputStream());
+			outSock = new DataOutputStream(socket.getOutputStream());
+		} catch (IOException ioe) {
+			System.out.println("Problemi nella creazione degli stream su socket: ");
+			ioe.printStackTrace();
+			System.exit(4);
+		}
+//	QUI VA CORPO DEL CLIENT, VANNO GESTITE INTERAZIONI CON VIEW
+/*		String cycleMessage = "\n^D(Unix)/^Z(Win) per terminare, altrimenti X: ";
+		System.out.print(cycleMessage);
+		String richiesta;
+
+		while ((richiesta = stdIn.readLine()) != null) {
+			outSock.writeUTF("Provola");
+			String result = inSock.readUTF();
+
+			System.out.print(cycleMessage);
+		}
+*/
+		// Libero le risorse
+		try {
+			outSock.flush();
+			socket.shutdownOutput();
+			socket.shutdownInput();
+		} catch (IOException e) {
+			System.out.println("Problemi nella chiusura della socket: ");
+			e.printStackTrace();
+			System.exit(5);
+		} catch (Exception e) {
+			System.out.println("Error: ");
+			e.printStackTrace();
+			System.exit(5);
+		}
+	}
+}

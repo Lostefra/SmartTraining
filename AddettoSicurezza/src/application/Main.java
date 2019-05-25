2fb8862a41835eca6067d77909f9ab7151339705
Lostefra <lostefra@users.noreply.github.com>
2019-05-25 20:00:16 +0200

    inizio addetto sicurezza

---
+++ AddettoSicurezza/src/application/Main.java
@@ -0,0 +1,30 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+	
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("/view/Login.fxml"));
+			Scene scene = new Scene(root,900,600);
+			primaryStage.setTitle("Smart Training - Addetto alla Sicurezza");
+			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			System.out.println("launched");
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}

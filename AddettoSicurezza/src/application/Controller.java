3fe9d8a22d993fa8f834aff8e66f9fafc8addd23
Lostefra <lostefra@users.noreply.github.com>
2019-05-26 12:04:24 +0200

    iniziato login addetto sicurezza

--- AddettoSicurezza/src/application/Controller.java
+++ AddettoSicurezza/src/application/Controller.java
@@ -1,17 +1,42 @@
 package application;
 
+import java.io.IOException;
+
+import accesso.LoginController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 //questo controller e' visto interamento dal fxml (deve essere interno al progetto)
 public class Controller {
 
 	@FXML
+	PasswordField password;
+	@FXML
+	TextField username;
+	
+	@FXML
     public void loginAddettoSicurezza(ActionEvent event)
     {
-		alert("OK","", "stampa");
+		if(username.getText().length() == 0 || password.getText().length() == 0)
+			return;
+		LoginController lc = new LoginController();
+		String result;
+		try {
+			result = lc.verificaCredenziali(username.getText(), password.getText());
+			if (result == null || !result.contentEquals("A")) {
+				alert("Errore","", "Le credenziali inserite non sono valide");
+				return;
+			}
+			//se sei qui l'addetto alla sicurezza è autenticato
+			// da qui bisogna caricare fxml della home
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		//inform("OK","", "benvenuto " + username.getText());
     }
 	
 	@FXML
@@ -28,6 +53,13 @@ public class Controller {
 		alert.showAndWait();
 	}
 
+	private static void inform(String title, String headerMessage, String contentMessage) {
+		Alert alert = new Alert(AlertType.INFORMATION);
+		alert.setTitle(title);
+		alert.setHeaderText(headerMessage);
+		alert.setContentText(contentMessage);
+		alert.showAndWait();
+	}
 
 	
 }

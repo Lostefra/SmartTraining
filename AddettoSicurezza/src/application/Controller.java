71686b5e3996c5e5ab0724198042dd1d2b3da7e5
Lostefra <lostefra@users.noreply.github.com>
2019-05-26 11:30:52 +0200

    inizio client addetto sicurezza

--- AddettoSicurezza/src/application/Controller.java
+++ AddettoSicurezza/src/application/Controller.java
@@ -1,22 +1,13 @@
 package application;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 
-public class Controller implements Initializable{
+//questo controller e' visto interamento dal fxml (deve essere interno al progetto)
+public class Controller {
 
-	@Override
-	public void initialize(URL arg0, ResourceBundle arg1) {
-		// TODO Auto-generated method stub
-		
-	}
-	
 	@FXML
     public void loginAddettoSicurezza(ActionEvent event)
     {
